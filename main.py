897ff777f1aa4d31ae97b7c491332a5e50192e4f
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2024-01-22 16:37:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from time import sleep as w, time as t
+from random import randint as r
+import pygame
+import sys
+from pygame.color import THECOLORS as col
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+while True:
+	pygame.display.flip()
